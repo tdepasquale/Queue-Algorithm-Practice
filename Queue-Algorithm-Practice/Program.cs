76dc33c0612b60b30edce1b02507c966262c1760
tdepasquale <t.depasquale@hotmail.com>
2019-08-29 18:07:43 -0400

    Add project files.

---
+++ Queue-Algorithm-Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Queue_Algorithm_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
